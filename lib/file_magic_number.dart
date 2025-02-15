9ee7811682c8572eff6f620e4075298acafc18e0
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-02-15 20:54:27 +0100

    Added web

--- lib/file_magic_number.dart
+++ lib/file_magic_number.dart
@@ -1,3 +1,3 @@
 export 'magic_number_stub.dart'
-    if (dart.library.io) 'magic_number_native.dart'
-    if (dart.library.html) 'magic_number_web.dart';
+    if (dart.library.io) 'file_magic_number_native.dart'
+    if (dart.library.html) 'file_magic_number_web.dart';
