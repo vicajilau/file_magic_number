a9415bf399892f8cfacbf81eddb33ceb9b7adc87
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-02-19 10:07:05 +0100

    Added documentation

--- test/file_extension.dart
+++ test/file_extension.dart
@@ -1,7 +1,14 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+/// An extension on the [File] class to add custom functionalities.
 extension FileExtension on File {
+  /// Writes a list of bytes to the file.
+  ///
+  /// This method converts the provided list of integers into a [Uint8List]
+  /// and writes it to the file asynchronously.
+  ///
+  /// [bytes] - A list of integers representing the byte data to be written to the file.
   void writeToFile(List<int> bytes) async {
     await writeAsBytes(Uint8List.fromList(bytes));
   }
